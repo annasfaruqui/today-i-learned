dda349d09b6ddbcf8ecc7f0e8f09d27229ad35d0
annasfaruqui <annasfaruqui123@gmail.com>
2026-07-10 14:25:21 +0530

    Updated supabase credentials

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://azeotvirgrmgpddjfknp.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6ZW90dmlyZ3JtZ3BkZGpma25wIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg4MTc1NjIsImV4cCI6MjAxNDM5MzU2Mn0.G6LoPUXUJ8VB8IJ6x6PgVSmPWSvBpr7VmhHFAUxhxCU";
+const supabaseUrl = "https://vewfghbgpsmkipzaeaun.supabase.co";
+const supabaseKey = "sb_publishable_5Shz3ZNWVQYrotYcn3tYNg_F2fpa054";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
